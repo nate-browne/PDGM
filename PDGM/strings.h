0a416f897f09c1b88bb984c1934437537992ce6d
Nate Browne <npcompletenate@protonmail.com>
2023-12-29 13:59:12 -0800

    adding strings header file

---
+++ PDGM/strings.h
@@ -0,0 +1,11 @@
+#ifndef STRINGS_H
+#define STRINGS_H
+
+#define WELCOME_STR "Welcome to %s!\n"
+#define GAMES_PROMPT "Enter number of simulations to run (default is 3): "
+#define RESULTS_STR "RESULTS:\np1 victories: %d\np2 victories: %d\nties: %d\nWinner: %s\n"
+#define REPEAT_STR "Go again? (y/n): "
+
+#define COMMAND_STR "%s: %s\n"
+
+#endif
