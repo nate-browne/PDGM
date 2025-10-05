dbbb969f40aca7e7f0d869060165e04461744dea
Nate Browne <npcompletenate@protonmail.com>
2025-10-04 17:03:52 -0700

    add a couple more strategies in

---
+++ strategies/always_defect.py
@@ -0,0 +1,11 @@
+"""
+This strategy is simple: always choose to defect no matter what.
+"""
+#!/usr/bin/env python3
+
+import sys
+
+if __name__ == '__main__':
+    while line := sys.stdin.readline().rstrip() != "quit":
+        print("1")
+
