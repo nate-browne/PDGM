e10144a92d38314a81e36b3a5fbfd8e1462f2c28
Nate Browne <npcompletenate@protonmail.com>
2025-10-05 20:46:48 -0700

    had an unnecessary file

--- PDGM/functions.h
+++ PDGM/functions.h
@@ -10,10 +10,6 @@ extern int32_t calculate_round_number(void);
 extern void init_game(GameState_t *);
 extern void init_running_count(RunningCount_t *);
 
-/* message.c */
-extern void broadcast_message(char *);
-extern char process_incoming_message(char *);
-
 /* game.c */
 extern int32_t run_game(GameState_t *, ParticipantProc_t *, ParticipantProc_t *);
 
