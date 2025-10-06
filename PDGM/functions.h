1f38e13be1e5c129f3c30497999010468cbd8ed2
Nate Browne <npcompletenate@protonmail.com>
2025-10-06 10:31:30 -0700

    change some params to be const

--- PDGM/functions.h
+++ PDGM/functions.h
@@ -19,8 +19,8 @@ extern int32_t run_game(GameState_t *, ParticipantProc_t *, ParticipantProc_t *)
 /* process.c */
 
 extern void destroy_participant_process(ParticipantProc_t *, int);
-extern ParticipantProc_t *start_participant_process(char *);
-extern void procprint(ParticipantProc_t *, char *);
+extern ParticipantProc_t *start_participant_process(const char *);
+extern void procprint(ParticipantProc_t *, const char *);
 extern void procread(ParticipantProc_t *, char *);
 
 #endif
