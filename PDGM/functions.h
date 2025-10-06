72efe0393733ab0bd0c2b1a81b5d1de8320709e6
Nate Browne <npcompletenate@protonmail.com>
2025-10-06 10:02:37 -0700

    reformat `functions.h`, add new strings/updates for `strings.h`

--- PDGM/functions.h
+++ PDGM/functions.h
@@ -4,16 +4,20 @@
 #include "pdgm.h"
 
 /* rand.c */
+
 extern int32_t calculate_round_number(void);
 
 /* init.c */
+
 extern void init_game(GameState_t *);
 extern void init_running_count(RunningCount_t *);
 
 /* game.c */
+
 extern int32_t run_game(GameState_t *, ParticipantProc_t *, ParticipantProc_t *);
 
 /* process.c */
+
 extern void destroy_participant_process(ParticipantProc_t *, int);
 extern ParticipantProc_t *start_participant_process(char *);
 extern void procprint(ParticipantProc_t *, char *);
--- PDGM/strings.h
+++ PDGM/strings.h
@@ -5,7 +5,9 @@
 #define GAMES_PROMPT "Enter number of simulations to run (up to %lu default is 50)\n\tWARNING: excessively large numbers may run _really_ slowly: "
 #define RESULTS_STR "\nRESULTS:\n\n\"%s\" victories: %d\n\"%s\" victories: %d\nties: %d\n\nWinner: %s\n"
 
-#define ROUND_STR "Round: %d\n"
+#define ROUND_STR "Round: %u\n"
 #define OPP_PREVIOUS_MOVE_STR "Opponent Previous Move: %d\n"
+
+#define GAME_STR "Game %lu\n"
 
 #endif
